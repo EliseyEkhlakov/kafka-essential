5dae5a44d911b2be3db8849328902272a43568eb
Елисей Ехлаков <elisey.ekhlakov@krit.pro>
2022-04-22 11:33:11 +0400

    Consumer with shutdown

--- kafka-basics/src/main/java/org/krit/ConsumerDemo.java
+++ kafka-basics/src/main/java/org/krit/ConsumerDemo.java
@@ -1,6 +1,9 @@
 package org.krit;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -9,6 +12,8 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+import java.util.Collections;
 import java.util.Properties;
 
 public class ConsumerDemo {
@@ -21,13 +26,31 @@ public class ConsumerDemo {
 
         String server = "localhost:9092";
         String groupId = "test-consumer";
+        String topic = "topic1";
 
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        consumer.subscribe(Collections.singletonList(topic));
+
+        while(true){
+
+            log.info("polling...");
+
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(100));
+
+            for(ConsumerRecord<String, String> record : records){
+                log.info("key: " + record.key() + " - value: " + record.value());
+                log.info("partition: " + record.partition() + " - offset: " + record.offset());
+            }
+        }
 
     }
 }
---
+++ kafka-basics/src/main/java/org/krit/ConsumerDemoWithShutdown.java
@@ -0,0 +1,79 @@
+package org.krit;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemoWithShutdown {
+
+    private final static Logger log = LoggerFactory
+            .getLogger(ConsumerDemoWithShutdown.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("start Consumer");
+
+        String server = "localhost:9092";
+        String groupId = "test-consumer with shutdown";
+        String topic = "topic1";
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        final Thread mainThread = Thread.currentThread();
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run(){
+                log.info("+++ Shutdown detected +++");
+                consumer.wakeup();
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        try{
+            consumer.subscribe(Collections.singletonList(topic));
+
+            while(true){
+
+                log.info("polling...");
+
+                ConsumerRecords<String, String> records =
+                        consumer.poll(Duration.ofMillis(100));
+
+                for(ConsumerRecord<String, String> record : records){
+                    log.info("key: " + record.key() + " - value: " + record.value());
+                    log.info("partition: " + record.partition() + " - offset: " + record.offset());
+                }
+            }
+
+        } catch (WakeupException e){
+            log.info("wake up exception...");
+        } catch (Exception e){
+            log.error("unexpected exception....");
+        } finally {
+            consumer.close();
+            log.info("consumer closed.");
+        }
+
+
+
+    }
+}
